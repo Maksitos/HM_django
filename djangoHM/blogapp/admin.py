ca52ea1f9e392b6bdf46e67c0b3fa71ab25c1903
Maksitos <Maksitos96@gmail.com>
2022-06-13 23:00:57 +0300

    HM 3

---
+++ djangoHM/blogapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Person, Article, Rating, Comment
+
+admin.site.register(Person)
+admin.site.register(Article)
+admin.site.register(Rating)
+admin.site.register(Comment)
+
+
+
---
+++ djangoHM/blogapp/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+
+class Article(models.Model):
+    author = models.ForeignKey(Person, on_delete=models.SET_DEFAULT, default=None, null=True, blank=True)
+    text = models.TextField(max_length=10000, null=True, blank=True)
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    text = models.TextField(max_length=10000, null=True)
+    subcomment = models.ForeignKey('blogapp.Comment', on_delete=models.CASCADE, null=True, blank=True, related_name='comments')
+
+
+class Rating(models.Model):
+
+    class Choice(models.IntegerChoices):
+        LIKE = 1
+        DISLIKE = 2
+
+    choice = models.IntegerField(choices=Choice.choices)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
